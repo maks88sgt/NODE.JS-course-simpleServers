6aae2280b74b9e0c9dcb0fa2aa70ed7abb561a42
maks88sgt <49103217+maks88sgt@users.noreply.github.com>
2020-12-27 16:44:01 +0300

    Add files via upload

---
+++ serverPostSender.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const options = {
+    hostname: 'localhost',
+    port: 3000,
+    method: 'POST',
+};
+
+const req = http.request(options, (response) => {
+    console.log(`${options.method} request sended.`)
+    console.log(`Response status code is ${ response.statusCode } (${ response.statusMessage }).`);
+});
+
+req.on('error', (e) => {
+    console.error(`
+                    problem with request: $ { e.message }
+                    `);
+});
+
+req.end();
